bd0a68bd64e8bef6f11fb91f3dcaaf56508325d6
Ali Ahmadi <ahmadi.world@gmail.com>
2024-11-13 18:14:10 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ const ali ={
   firstName: "ali",
   Lastname:"ahmadi",
   age:32,
+  bindP:function(){
+    return console.log(`${hi} ${ohmygod}  my name is ${this.firstName} ${this.Lastname}, I Am ${this.age} years old`)
+
+  }
 };
 
 
@@ -19,4 +23,4 @@ person.call(hasan,"Hello","!");
 person.call(ali,"Hello","!");
 let alijon= person.bind(ali,"Hello","!");
 
-console.log(alijon());
+console.log( alijon());
